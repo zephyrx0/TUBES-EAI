92df57133569f689240db07025a18a42db69eb7c
zephyrx0 <ganelajeisa@gmail.com>
2024-06-09 18:12:29 +0700

    crud dokter
    # Conflicts:
    #	dokter/app.py
    #	main/app.py
    #	main/templates/alat_medis.html
    #	main/templates/dokter.html
    #	main/templates/klinik.html
    #	main/templates/sidebar.html

--- main/app.py
+++ main/app.py
@@ -1,6 +1,5 @@
-from flask import Flask , request, render_template, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import requests
-
 
 app = Flask(__name__)
 
@@ -62,54 +61,7 @@ def get_pasien():
     response = requests.get('http://127.0.0.1:5001/pasien')
     return response.json()
 
-def get_pasien_by_id(pasien_id):
-    response = requests.get(f'http://127.0.0.1:5001/detailpasien?pasien_id={pasien_id}')
-    return response.json()
-
-@app.route('/pasien')
-def pasien_page():
-    pasien = get_pasien()
-    return render_template('pasien.html', pasien=pasien)
-
-@app.route('/detailpasien/<int:pasien_id>', methods=['GET'])
-def detail_pasien_page(pasien_id):
-    pasien = get_pasien_by_id(pasien_id)
-    if pasien:
-        return render_template('detail_pasien.html', pasien=pasien)
-    else:
-        return "Data pasien tidak ditemukan", 404
-
-@app.route('/pasien/add', methods=['GET', 'POST'])
-def add_pasien_form():
-    if request.method == 'POST':
-        new_pasien = request.form.to_dict()
-        response = requests.post('http://127.0.0.1:5001/pasien', json=new_pasien)
-        return redirect(url_for('pasien_page'))
-    return render_template('add_pasien.html')
-
-@app.route('/pasien/edit/<int:pasien_id>', methods=['GET','POST'])
-def update_pasien(pasien_id):
-    pasien = get_pasien_by_id(pasien_id)
-    if request.method == 'POST':
-        updated_pasien = request.form.to_dict()
-        response = requests.put(f'http://127.0.0.1:5001/editpasien/{pasien_id}', json=updated_pasien)
-        return redirect(url_for('pasien_page'))
-    return render_template('edit_pasien.html', pasien=pasien)
-
-@app.route('/pasien/<int:pasien_id>', methods=['DELETE'])
-def delete_pasien(pasien_id):
-    if request.method == 'DELETE':
-        response = requests.delete(f'http://127.0.0.1:5001/pasien/{pasien_id}')
-        return jsonify(response.json())
-
-#layanan dokter
-
-def get_dokter():
-    response = requests.get('http://127.0.0.1:5002/dokter')
-    return response.json()
-
-#layanan janji
-
+# layanan janji
 def get_janji():
     response = requests.get('http://127.0.0.1:5003/janji')
     return response.json()
@@ -143,95 +95,26 @@ def get_alat_medis():
 def home():
     return render_template('home.html', active_page='home')
 
+@app.route('/pasien')
+def pasien_page():
+    return render_template('pasien.html', active_page='pasien_page')
 
-
-@app.route('/dokter')
-def dokter_page():
-    dokters = get_dokter()
-    return render_template('dokter.html', dokters=dokters)
-
-@app.route('/delete_dokter/<int:dokter_id>')
-def delete_dokter(dokter_id):
-    response = requests.delete(f'http://localhost:5002/delete_dokter/{dokter_id}')
-    return redirect(url_for('dokter_page'))
 
 @app.route('/janji')
 def janji_page():
-    pasien = {p['pasien_id']: p['nama'] for p in get_pasien()}
-    dokter = {d['dokter_id']: d['nama'] for d in get_dokter()}
-    janji = get_janji()
-
-    # Enrich janji data with patient and doctor names
-    for j in janji:
-        j['nama_pasien'] = pasien.get(j['pasien_id'], 'Unknown')
-        j['nama_dokter'] = dokter.get(j['dokter_id'], 'Unknown')
-    return render_template('janji.html', janji=janji, pasien=pasien, dokter=dokter)
+    return render_template('janji.html', active_page='janji_page')
 
 @app.route('/riwayat')
 def riwayat_page():
-    pasien = {p['pasien_id']: p['nama'] for p in get_pasien()}
-    riwayat = get_riwayat()
-
-    for r in riwayat:
-        r['nama_pasien'] = pasien.get(r['pasien_id'], 'Unknown')
-    return render_template('riwayat.html', riwayat=riwayat)
+    return render_template('riwayat.html', active_page='riwayat_page')
 
 @app.route('/rawat_inap')
 def rawat_inap_page():
-    pasien = {p['pasien_id']: p['nama'] for p in get_pasien()}
-    dokter = {d['dokter_id']: d['nama'] for d in get_dokter()}
-    rawat_inap = get_rawat_inap()
-
-    # Enrich janji data with patient and doctor names
-    for r in rawat_inap:
-        r['nama_pasien'] = pasien.get(r['pasien_id'], 'Unknown')
-        r['nama_dokter'] = dokter.get(r['dokter_id'], 'Unknown')
-    return render_template('rawat_inap.html', rawat_inap=rawat_inap)
-
-def get_klinik():
-    response = requests.get('http://127.0.0.1:5006/klinik')
-    return response.json()
-
-def get_klinik_id(klinik_id):
-    response = requests.get('http://127.0.0.1:5006/api/klinik/{klinik_id}')
-    return response.json()
+    return render_template('rawat_inap.html', active_page='rawat_inap_page')
 
 @app.route('/klinik')
 def klinik_page():
-    klinik = get_klinik()
-    return render_template('klinik.html', klinik=klinik)
-
-@app.route('/addklinik')
-def tambah_klinik():
-    return render_template('tambah_klinik.html')
-
-@app.route('/add_klinik', methods=['POST'])
-def add_klinik():
-    data = request.form
-    response = requests.post('http://127.0.0.1:5006/klinik', json=data)
-    return redirect(url_for('klinik_page'))
-
-#form edit
-@app.route('/editklinik/<int:klinik_id>')
-def edit_klinik(klinik_id):
-    klinikk = get_klinik_id(klinik_id)
-    return render_template('edit_klinik.html', klinik_id=klinik_id, klinikk=klinikk)
-
-@app.route('/update_klinik/<int:klinik_id>', methods=['GET', 'POST'])
-def update_klinik(klinik_id):
-    if request.method == 'GET':
-        response = requests.get(f'http://127.0.0.1:5006/klinik/{klinik_id}')
-        klinik = response.json()
-        return render_template('edit_klinik.html', klinik=klinik)
-    elif request.method == 'POST':
-        data = request.form
-        response = requests.put(f'http://127.0.0.1:5006/klinik/{klinik_id}', json=data)
-        return redirect(url_for('klinik_page'))
-
-@app.route('/delete_klinik/<int:klinik_id>')
-def delete_klinik(klinik_id):
-    response = requests.delete(f'http://localhost:5006/delete_klinik/{klinik_id}')
-    return redirect(url_for('klinik_page'))
+    return render_template('klinik.html', active_page='klinik_page')
 
 @app.route('/obat')
 def obat_page():
@@ -239,10 +122,7 @@ def obat_page():
 
 @app.route('/alat_medis')
 def alat_medis_page():
-    medis = get_alat_medis()
-    return render_template('alat_medis.html',medis=medis)
-    
-    
+    return render_template('alat_medis.html', active_page='alat_medis_page')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
